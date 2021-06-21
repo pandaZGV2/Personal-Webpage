e1a12d84fc014b73cb493ef95742417531cf5543
PandaZGV2 <aaronmonis22@gmail.com>
2021-06-21 11:38:50 +0400

    Added images gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,7 @@
+var key = 0;
 function CycleImages(name) {
     console.log(name)
-  switch (name) {
-    case "./images/desktop.png":
-      document.getElementById("gallery").getAttribute("src") = "./images/desktop1.png";
-      break;
-    case "./images/desktop1.png":
-      document.getElementById("gallery").getAttribute("src") = "./images/desktop2.png";
-      break;
-    case "./images/desktop2.png":
-      document.getElementById("gallery").getAttribute("src") = "./images/desktop3.png";
-      break;
-    case "./images/desktop3.png":
-      document.getElementById("gallery").getAttribute("src") = "./images/desktop4.png";
-      break;
-    case "./images/desktop4.png":
-      document.getElementById("gallery").getAttribute("src") = "./images/desktop.png";
-      break;
-  }
+    key = (key+1)%5;
+    var newname = "./images/desktop" + key + ".png";
+    document.getElementById("gallery").src = newname;
 }
