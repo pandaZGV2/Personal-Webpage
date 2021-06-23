8f4359d584d0b063d9167af65642c72ab300198e
PandaZGV2 <aaronmonis22@gmail.com>
2021-06-23 12:01:54 +0400

    Added colour change functionality to navbar elems

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 var key = 0;
 function CycleImages(name) {
-    console.log(name)
-    key = (key+1)%5;
-    var newname = "./images/desktop" + key + ".png";
-    document.getElementById("gallery").src = newname;
+  console.log(name);
+  key = (key + 1) % 5;
+  var newname = "./images/desktop" + key + ".png";
+  document.getElementById("gallery").src = newname;
+}
+function navChange(id) {
+  document.getElementById(id).style.color = "rgb(209, 204, 204)";
+  document.getElementById(id).style.transition = "color 1s ease";
+}
+function navrevert(id) {
+  document.getElementById(id).style.color = "white";
+  document.getElementById(id).style.transition = "color 1s ease";
 }
