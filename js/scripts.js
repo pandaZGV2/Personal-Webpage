a7fd91ceff8e087d6ce9a72ca140ab0b686afa0b
PandaZGV2 <aaronmonis22@gmail.com>
2021-06-30 15:36:07 +0400

    move html files to html/

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var key = 0;
 function CycleImages(name) {
   console.log(name);
   key = (key + 1) % 5;
-  var newname = "./images/desktop" + key + ".png";
+  var newname = "../images/desktop" + key + ".png";
   document.getElementById("gallery").src = newname;
 }
 function navChange(id) {
