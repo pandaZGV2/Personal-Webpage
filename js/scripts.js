af1989987c5c3d997e4c6ab5ec0781ec0371f698
PandaZGV2 <aaronmonis22@gmail.com>
2021-07-01 11:59:55 +0400

    Removing footer from contact.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var key = 0;
 function CycleImages(name) {
-  console.log(name);
+  // console.log(name);
   key = (key + 1) % 5;
   var newname = "../images/desktop" + key + ".png";
   document.getElementById("gallery").src = newname;
